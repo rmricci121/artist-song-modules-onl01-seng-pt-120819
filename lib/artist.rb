203183010c196c44180b167cbc5511d5a5730a7d
Robert M Ricci <rmricci121@gmail.com>
2020-01-22 10:22:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Artist
   
   include Paramable
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
